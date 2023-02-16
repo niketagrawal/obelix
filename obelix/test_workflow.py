af163ffe968788b8f6b934f3e6a35be309b2e5f1
AdrianM0 <adrianmirza81@gmail.com>
2023-02-16 21:32:09 +0100

    Minor changes, fixing chemspax input.

--- obelix/test_workflow.py
+++ obelix/test_workflow.py
@@ -1,6 +1,6 @@
 from run_workflow import *
 
-ligand_excel_file = os.path.join(os.getcwd(), 'ligand_test.xlsx')
+ligand_excel_file = os.path.join(os.getcwd(), 'test_mace.xlsx')
 auxiliary_ligands = ['CC#[N:1]', 'CC#[N:1]']
 substrate = ['CC#[N:1]']
 
@@ -21,21 +21,20 @@ current_directory = os.path.join(os.getcwd(), "chemspax")  # use path.join()
 # os.chdir(current_directory)
 path_to_substituents = os.path.join(current_directory, "substituents_xyz") 
 path_to_database = os.path.join(path_to_substituents, "central_atom_centroid_database.csv")
-substituent_df = pd.read_excel('demo_chemspax_cv.xlsx').dropna()
+substituent_df = pd.read_excel('test_chemspax.xlsx').dropna()
 substituent_list = np.array(substituent_df[['R1', 'R2', 'R3', 'R4']])
 # print(substituent_list)
 names = substituent_df['Name']
-skeleton_list = substituent_df['Functionalization']
+func_list = substituent_df['Functionalization']
 # print(skeleton_list)
 path_to_hand_drawn_skeletons = os.path.join(current_directory, "skeletons")
 path_to_output = os.path.join(current_directory, "complexes")
 
-chemspax_input = {'skeleton_list' : skeleton_list, 
-                'substituent_list' : substituent_list, 
+chemspax_input = {'substituent_list' : substituent_list, 
                 'path_to_database' : path_to_database, 
                 'path_to_substituents' : path_to_substituents}
 
 workflow = Workflow(mace_input = mace_input, chemspax_input=chemspax_input, path_to_workflow = os.getcwd() + '/wf_test5', geom='BD')
 workflow.prepare_folder_structure()
 workflow.run_mace()
-workflow.run_chemspax(names=names, functionalization_list=skeleton_list)
+workflow.run_chemspax(names=names, functionalization_list=func_list)
