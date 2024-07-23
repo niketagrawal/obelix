484a1444244324415a73aca8b8f9ffb83a9206f7
Niket <15953349+niketagrawal@users.noreply.github.com>
2024-07-23 11:05:12 +0200

    Parametrization of the testcases to cover different cases keeps code readable and maintainable. To check: testcase assert descriptor values fails for nbd gaussian case because the numpy array contains a boolean

--- tests/test_descriptor_calculator/test_descriptor_calculator.py
+++ tests/test_descriptor_calculator/test_descriptor_calculator.py
@@ -8,18 +8,33 @@ from obelix.descriptor_calculator import Descriptors
 
 @pytest.fixture
 def setup_descriptors(request):
-    scenario = request.param
-    path_to_xyz_files = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), "data", "xyz_files", scenario
+    metal_adduct, output_type = request.param
+    path_to_input = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        "data",
+        metal_adduct,
+        output_type,
     )
     descriptors = Descriptors(
         central_atom="Rh",
-        path_to_workflow=path_to_xyz_files,
-        output_type="xyz",
+        path_to_workflow=path_to_input,
+        output_type=output_type,
     )
-    descriptors.calculate_morfeus_descriptors(
-        geom_type="BD", solvent=None, printout=False, metal_adduct=scenario
-    )
+
+    if output_type == "xyz" or output_type == "crest":
+        descriptors.calculate_morfeus_descriptors(
+            geom_type="BD", solvent=None, printout=False, metal_adduct=metal_adduct
+        )
+    else:
+        descriptors.calculate_dft_descriptors_from_log(
+            geom_type="BD",
+            solvent=None,
+            extract_xyz_from_log=True,
+            printout=False,
+            metal_adduct=metal_adduct,
+            plot_steric_map=False,
+        )
+
     output_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "output")
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -28,21 +43,34 @@ def setup_descriptors(request):
     output_file_path = os.path.join(output_dir, "descriptors.csv")
     descriptors.descriptor_df.to_csv(output_file_path, index=False)
 
-    yield descriptors, scenario
+    yield descriptors, metal_adduct, output_type
 
     # Cleanup: Delete the output folder and all its contents
     shutil.rmtree(output_dir)
 
 
-@pytest.mark.parametrize("setup_descriptors", ["nbd", "pristine"], indirect=True)
+@pytest.mark.parametrize(
+    "setup_descriptors",
+    [
+        ("nbd", "xyz"),
+        ("pristine", "xyz"),
+        ("nbd", "gaussian"),
+    ],
+    indirect=True,
+    ids=[
+        "nbd-xyz",
+        "pristine-xyz",
+        "nbd-gaussian",
+    ],  # Custom IDs for each parameter set
+)
 class TestDescriptorCalculation:
     def test_descriptor_values(self, setup_descriptors):
         # path to expected csv is descriptors_nbd.csv or descriptors_pristine.csv
-        descriptors, scenario = setup_descriptors
+        descriptors, metal_adduct, output_type = setup_descriptors
         path_expected_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
             "expected_output",
-            f"descriptors_{scenario}.csv",
+            f"descriptors_{metal_adduct}_{output_type}.csv",
         )
         output_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
@@ -70,11 +98,11 @@ class TestDescriptorCalculation:
 
     def test_index_values(self, setup_descriptors):
         # path to expected csv is descriptors_nbd.csv or descriptors_pristine.csv
-        descriptors, scenario = setup_descriptors
+        descriptors, metal_adduct, output_type = setup_descriptors
         path_expected_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
             "expected_output",
-            f"descriptors_{scenario}.csv",
+            f"descriptors_{metal_adduct}_{output_type}.csv",
         )
         output_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
@@ -98,11 +126,11 @@ class TestDescriptorCalculation:
 
     def test_element_values(self, setup_descriptors):
         # path to expected csv is descriptors_nbd.csv or descriptors_pristine.csv
-        descriptors, scenario = setup_descriptors
+        descriptors, metal_adduct, output_type = setup_descriptors
         path_expected_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
             "expected_output",
-            f"descriptors_{scenario}.csv",
+            f"descriptors_{metal_adduct}_{output_type}.csv",
         )
         output_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
@@ -125,11 +153,11 @@ class TestDescriptorCalculation:
 
     def test_filename_values(self, setup_descriptors):
         # path to expected csv is descriptors_nbd.csv or descriptors_pristine.csv
-        descriptors, scenario = setup_descriptors
+        descriptors, metal_adduct, output_type = setup_descriptors
         path_expected_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
             "expected_output",
-            f"descriptors_{scenario}.csv",
+            f"descriptors_{metal_adduct}_{output_type}.csv",
         )
         output_csv = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
